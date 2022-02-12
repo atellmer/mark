df4f51710fe0d21d9ecb5a3ab4fc01c1fefc3a5a
atellmer2 <aromanov@seeneco.ru>
2022-02-12 11:00:50 +0500

    fix

--- src/ui/modules/trading-tester/components/entry/entry.view.tsx
+++ src/ui/modules/trading-tester/components/entry/entry.view.tsx
@@ -9,31 +9,13 @@ import trainCancerDataset from '@core/ai/datasets/cancer/train.csv';
 import testCancerDataset from '@core/ai/datasets/cancer/test.csv';
 import trainTicTacToeDataset from '@core/ai/datasets/tic-tac-toe/train.csv';
 import testTicTacToeDataset from '@core/ai/datasets/tic-tac-toe/test.csv';
-//import pnnIrisesInlineEngine from '@core/ai/pnn/trained/irises-model.json';
-//import pnnCarnumbersInlineEngine from '@core/ai/pnn/trained/carnumbers-model.json';
-//import adaboostIrisesInlineEngine from '@core/ai/adaboost/trained/irises-model.json';
-import { pnn } from '@core/ai/pnn';
-import { adaboost } from '@core/ai/adaboost';
 import { TradingTester } from '../tester';
 
 (() => {
 	const trainSamples = Sample.fromDataset(trainIrisesDataset);
 	const testSamples = Sample.fromDataset(testIrisesDataset);
 
-	// const pnnEngine = pnn({ samples: trainSamples });
 
-	// pnnEngine.verasity(trainSamples, testSamples);
-
-	console.log('start...');
-	setTimeout(() => {
-		const adaboostEngine = adaboost({
-			samples: trainSamples,
-			estimatorsTotal: 100,
-			samplingSize: 10,
-			enableLogs: true,
-		});
-		adaboostEngine.verasity(trainSamples, testSamples);
-	}, 3000);
 })();
 
 export type TradingTesterEntryProps = {};
